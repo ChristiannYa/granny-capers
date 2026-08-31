1a4bd9f29665368d2ea21554638ed32e8fc29db2
ChristiannYa <christian.ylopez02@gmail.com>
2026-08-31 17:44:47 -0400

    7.5-orbiting-instant

--- scenes/granny/granny.gd
+++ scenes/granny/granny.gd
@@ -6,11 +6,13 @@ const _ROTATE_LERP := 8.0
 const _SPEED := 5.0
 const _DECELERATION := 30.0
 const _JUMP_VELOCITY := 12.5
+const _CAM_ROTATION_SPEED := PI
 
 @onready var jump_sound: AudioStreamPlayer = $JumpSound
 @onready var land_sound: AudioStreamPlayer = $LandSound
 @onready var debug_label: Label3D = $DebugLabel
 @onready var body: MeshInstance3D = $Body
+@onready var camera_controller: Node3D = $CameraController
 
 ## Tracks whether on the previous frame we were on the floor or not
 var _last_on_floor := false
@@ -19,12 +21,18 @@ func _physics_process(delta: float):
 	apply_gravity(delta)
 	handle_jump()
 	handle_movement(delta)
+	handle_camera(delta)
 	move_and_slide()
 	check_landing()
 
+func handle_camera(delta: float):
+	var cam_turn: float = Input.get_axis("cam_right", "cam_left")
+	debug_label.text += "\ncam: %.2f" % cam_turn
+	camera_controller.rotate_y(cam_turn * delta * _CAM_ROTATION_SPEED)
+
 func handle_movement(delta: float):
 	var input_dir: Vector2 = Input.get_vector("m_left", "m_right", "m_fwd", "m_back")
-	var dir := Vector3(input_dir.x, 0.0, input_dir.y)
+	var dir := camera_controller.basis * Vector3(input_dir.x, 0.0, input_dir.y)
 	debug_label.text = ("input: (%.1v)\n" + "dir: (%.1v)") % [ 
 		input_dir, dir
 	]
