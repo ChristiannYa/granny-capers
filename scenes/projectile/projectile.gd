5069e89753603e3946c2e2bd94771d26d72ba20d
ChristiannYa <christian.ylopez02@gmail.com>
2026-09-01 16:39:31 -0400

    15-projectile

---
+++ scenes/projectile/projectile.gd
@@ -0,0 +1,16 @@
+class_name Projectile
+extends Node3D
+@export var gravity := 1.0
+@export var speed := 4.0
+@export var spin_speed := 16.0
+
+var _velocity := Vector3.ZERO
+
+func _ready():
+	# Negative because Granny is facing the opposite z direction
+	_velocity = -global_transform.basis.z * speed
+
+func _physics_process(delta: float):
+	_velocity.y += -(gravity * delta)
+	global_position += _velocity * delta
+	rotate_object_local(-Vector3.RIGHT, spin_speed * delta)
