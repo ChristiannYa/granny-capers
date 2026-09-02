604f1be8bccce6304caba96b419d05e9ee84753b
ChristiannYa <christian.ylopez02@gmail.com>
2026-09-02 16:09:05 -0400

    21-explosion-effect

--- scenes/explosion_effect/explosion_effect.gd
+++ scenes/explosion_effect/explosion_effect.gd
@@ -2,7 +2,11 @@ extends Node3D
 
 @export var audio_stream: AudioStream
 
-func _ready() -> void:
-	SoundManager.play_3d(audio_stream, global_position)
-	queue_free()
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready():
+	animation_player.play("run")
+	SoundManager.play_3d(audio_stream, global_position)
+
+func _on_animation_player_animation_finished(_anim_name: StringName):
+	queue_free()
